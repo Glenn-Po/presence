333d00055396ac6a00ba6ded72e0062d0647169e
glenn-po <glennfor72@gmail.com>
2023-04-30 02:42:43 +0100

    Add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
+# from django.contrib.gis.db import models
+from datetime import datetime
+import uuid
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=128, blank=False, null=False)
+    code = models.CharField(default=uuid.uuid4(), editable=False)
+    latitude = models.DecimalField(decimal_places=10, blank=False, null=False)
+    longitude = models.DecimalField(decimal_places=10, blank=False, null=False)
+    radius = models.IntegerField(default=50, blank=False, null=False)
+    qr_code_url = models.TextField(blank=False, null=False)
+
+
+class Attendance(models.Model):
+    ATTENDANCE_TYPE = (('CI', 'Clocked In'), ('CO', 'Clocked Out'))
+
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_time = models.DateTimeField(default=datetime.now(), null=False)
+    type = models.CharField(choices=ATTENDANCE_TYPE, blank=False, null=False)
