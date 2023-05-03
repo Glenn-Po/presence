0f236f4eeefda88ade453bbcef8d793e21338b6c
glenn-po <glennfor72@gmail.com>
2023-05-03 06:09:23 +0100

    Add custom builder for vercel

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,10 @@
 # pip install python3-venv
 # python3.9 -m venv .
 # source bin/activate
-apt-get install mysql-client
-pip3 install --upgrade venv
-python3.9 -m venv .
-source ./bin/activate
+# apt-get install mysql-client
+# pip3 install --upgrade venv
+# python3.9 -m venv .
+# source ./bin/activate
 pip3 install --upgrade pip
-pip3 install wheel
-pip3 install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
