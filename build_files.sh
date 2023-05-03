e8c320b1724b1c68cae44426b40bc36dc2b47f76
glenn-po <glennfor72@gmail.com>
2023-05-03 05:07:02 +0100

    config: download venv on server

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 # build_files.sh
-# python3.9 -m venv .
-# source bin/activate
+sudo apt update
+sudo apt install python3-venv
+python3.9 -m venv .
+source bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
