41fac4d0f0fd47a3f9dd769ec9df5ebe12629a12
glenn-po <glennfor72@gmail.com>
2023-05-03 05:29:29 +0100

    install mysqlclient and wheel

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,11 @@
 # pip install python3-venv
 # python3.9 -m venv .
 # source bin/activate
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.15-1_all.deb
+sudo apt-get update
+sudo apt-get install mysql-client
 pip install --upgrade pip
+pip install wheel
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
