66ea3eceb6bd752a671ebd7a64ca0d7f1e7dd8a9
glenn-po <glennfor72@gmail.com>
2023-05-03 05:18:15 +0100

    remove virtual env

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 # build_files.sh
-pip install python3-venv
-python3.9 -m venv .
-source bin/activate
+# pip install python3-venv
+# python3.9 -m venv .
+# source bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
