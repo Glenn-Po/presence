5a4ecab9dfcde33f28f3c80cb599ab26494955d1
glenn-po <glennfor72@gmail.com>
2023-05-03 05:38:50 +0100

    re-setup venv

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,9 @@
 # pip install python3-venv
 # python3.9 -m venv .
 # source bin/activate
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
-sudo dpkg -i mysql-apt-config_0.8.15-1_all.deb
-sudo apt-get update
-sudo apt-get install mysql-client
+pip install venv
+python3.9 -m venv .
+source ./bin/activate
 pip install --upgrade pip
 pip install wheel
 pip install -r requirements.txt
